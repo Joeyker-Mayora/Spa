e634291c0ec9238fae65c0672f5f045be14044e5
Joeyker <oswaldo_avilez@outlook.es>
2023-05-17 18:59:04 -0300

    joeyker

--- src/Helpers/GetGifs/Getgifs.jsx
+++ src/Helpers/GetGifs/Getgifs.jsx
@@ -1,7 +1,7 @@
 export const Getgifs = async(category) => {
     
 
-    const resp = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=A3qxK1O1X6ljTeuUwDwBY2rdzqr3AJ4N&q=${category}&limit=12`);
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=A3qxK1O1X6ljTeuUwDwBY2rdzqr3AJ4N&q=${category}&limit=12`);
     const {data} = await resp.json()
     
     const gifs = data.map( img => ({
